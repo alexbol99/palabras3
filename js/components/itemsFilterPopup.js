385a27015b516d52f5a802c5831bf52281f7361d
alexbol99 <alexbol99@gmail.com>
2015-05-11 17:18:49 +0300

    Item Filters Popup - category selection added

--- js/components/itemsFilterPopup.js
+++ js/components/itemsFilterPopup.js
@@ -3,12 +3,46 @@
  */
 define([],
     function () {
+        var MySelect = React.createClass({
+            categoryChanged: function(event) {
+                console.log(event.target.value)
+            },
+            render: function () {
+                var Input = ReactBootstrap.Input;
+                var ListGroupItem = ReactBootstrap.ListGroupItem;
+                var options = this.props.data.map(function (item) {
+                    return (
+                        <option bsStyle='success' value={item.get('category')} key={item.get('category')}>
+                            {item.get('category') + " (" + item.get('count') + ')'}
+                        </option>
+                    )
+                });
+                const selectInstance = (
+                    <form>
+                        <ReactBootstrap.Input type='select' placeholder='select' onChange={this.props.onCategorySelected}>
+                            {options}
+                        </ReactBootstrap.Input>
+                    </form>
+                );
+                return selectInstance;
+            }
+        });
+
         const ItemsFilterPopup = React.createClass({
-            render() {
+            render: function() {
+                const myselectInstance = (
+                    <MySelect data={this.props.categories} ref="categories"
+                        onCategorySelected = {this.props.onCategorySelected}
+                    />
+                );
+
                 return (
-                    <ReactBootstrap.Modal {...this.props} bsStyle='primary' title='Modal heading' animation={false}>
+                    <ReactBootstrap.Modal {...this.props} bsStyle='primary' title='Filter dictionary' animation={false}>
                         <div className='modal-body'>
                             <h4>Text in a modal</h4>
+
+                            {myselectInstance}
+
                         </div>
                         <div className='modal-footer'>
                             <ReactBootstrap.Button onClick={this.props.onRequestHide}>Close</ReactBootstrap.Button>
--- js/components/toolbar.js
+++ js/components/toolbar.js
@@ -4,49 +4,6 @@
 
 define(['components/itemsFilterPopup', 'components/infoPopup'],
     function (ItemsFilterPopup, InfoPopup) {
-        var MyList = React.createClass({
-            render: function () {
-                var ListGroup = ReactBootstrap.ListGroup;
-                var ListGroupItem = ReactBootstrap.ListGroupItem;
-                var list = this.props.data.map(function (item) {
-                    return (
-                        <ListGroupItem bsStyle='success' key={item.get('category')}>
-                            {item.get('category')}
-                            <ReactBootstrap.Badge>{item.get('count')}</ReactBootstrap.Badge>
-                        </ListGroupItem>
-                    )
-                });
-                const listgroupInstance = (
-                    <ListGroup>{list}</ListGroup>
-                );
-                return listgroupInstance;
-            }
-        });
-
-        var MySelect = React.createClass({
-            categoryChanged: function(event) {
-                console.log(event.target.value)
-            },
-            render: function () {
-                var Input = ReactBootstrap.Input;
-                var ListGroupItem = ReactBootstrap.ListGroupItem;
-                var options = this.props.data.map(function (item) {
-                    return (
-                        <option bsStyle='success' value={item.get('category')} key={item.get('category')}>
-                            {item.get('category') + " (" + item.get('count') + ')'}
-                        </option>
-                    )
-                });
-                const selectInstance = (
-                    <form>
-                        <ReactBootstrap.Input type='select' placeholder='select' onChange={this.props.onCategorySelected}>
-                            {options}
-                        </ReactBootstrap.Input>
-                    </form>
-                );
-                return selectInstance;
-            }
-        });
 
         var Toolbar = React.createClass({
             categoryChanged: function(event) {
@@ -61,17 +18,13 @@ define(['components/itemsFilterPopup', 'components/infoPopup'],
                 var ButtonToolbar = ReactBootstrap.ButtonToolbar;
                 var Button = ReactBootstrap.Button;
 
-                const myselectInstance = (
-                    <MySelect data={this.props.categories} ref="categories"
-                        onCategorySelected = {this.props.onCategorySelected}
-                    />
-                );
+/*
                 var catButton = "";
                 if (this.props.selectedCategory.get) {
                     var catButton = this.props.selectedCategory.get("category") +
                         ' (' + this.props.selectedCategory.get("count") + ')';
                 }
-
+*/
                 var glyphSound = (this.props.sound == "on" ? "volume-up" : "volume-off");
                 /*
                 <ReactBootstrap.ButtonGroup>
@@ -80,12 +33,20 @@ define(['components/itemsFilterPopup', 'components/infoPopup'],
                     </Button>
                 </ReactBootstrap.ButtonGroup>
                 */
+
+                const itemsFilterPopupInstance = (
+                    <ItemsFilterPopup
+                        categories = {this.props.categories}
+                        onCategorySelected = {this.props.onCategorySelected}
+                    />
+                );
+
                 const toolbarInstance = (
                     <div>
                         <ReactBootstrap.ButtonGroup justified>
 
                             <ReactBootstrap.ButtonGroup>
-                                <ReactBootstrap.ModalTrigger modal={<ItemsFilterPopup />}>
+                                <ReactBootstrap.ModalTrigger modal={itemsFilterPopupInstance}>
                                     <Button bsStyle='primary' bsSize='medium' onClick={this.props.onClickFilterButton}>
                                         <ReactBootstrap.Glyphicon glyph='filter' />
                                     </Button>
@@ -113,7 +74,7 @@ define(['components/itemsFilterPopup', 'components/infoPopup'],
                             </ReactBootstrap.ButtonGroup>
 
                         </ReactBootstrap.ButtonGroup>
-                        {myselectInstance}
+
                     </div>
                 );
                 return toolbarInstance;
