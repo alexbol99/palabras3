385bc623be38e3bd99ff2fd65766595cd825674f
alexbol99 <alexbol99@gmail.com>
2015-07-27 17:33:56 +0300

    Move fb script loading to index.html for faster loading ...

--- js/models/fb.js
+++ js/models/fb.js
@@ -18,29 +18,35 @@ define([],
                     self.trigger("fbInitialized");
                 }
                 else {
-                    window.fbAsyncInit = function () {
-                        Parse.FacebookUtils.init({
-                            appId: '398066583702762',
-                            cookie: true,  // enable cookies to allow Parse to access the session
-                            xfbml: false, // initialize Facebook social plugins on the page - do not check for plugin, not using them
-                            version: 'v2.3' // point to the latest Facebook Graph API version
-                        });
-
-                        // Run code after the Facebook SDK is loaded.
+                    if (_gIsFBLoaded) {
                         self.set('initialized', true);
                         self.trigger("fbInitialized");
-                    };
+                    }
+                    else {
+                        window.fbAsyncInit = function () {
+                            Parse.FacebookUtils.init({
+                                appId: '398066583702762',
+                                cookie: true,  // enable cookies to allow Parse to access the session
+                                xfbml: false, // initialize Facebook social plugins on the page - do not check for plugin, not using them
+                                version: 'v2.3' // point to the latest Facebook Graph API version
+                            });
 
-                    (function (d, s, id) {
-                        var js, fjs = d.getElementsByTagName(s)[0];
-                        if (d.getElementById(id)) {
-                            return;
-                        }
-                        js = d.createElement(s);
-                        js.id = id;
-                        js.src = "//connect.facebook.net/en_US/sdk.js";
-                        fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));
+                            // Run code after the Facebook SDK is loaded.
+                            self.set('initialized', true);
+                            self.trigger("fbInitialized");
+                        };
+
+                        (function (d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) {
+                                return;
+                            }
+                            js = d.createElement(s);
+                            js.id = id;
+                            js.src = "//connect.facebook.net/en_US/sdk.js";
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));
+                    }
                 }
             },
             auth: function() {
